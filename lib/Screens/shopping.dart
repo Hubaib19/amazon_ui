6c7011d4007d5251299a0f6f20015e981a9114f8
Hubaib75 <hubaibgit003@gmail.com>
2023-10-25 09:43:24 +0530

    first commit

---
+++ lib/Screens/shopping.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Shopping extends StatelessWidget {
+  const Shopping({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink
+    );
+  }
+}
